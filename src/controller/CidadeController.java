f360043ae504ef98a252486bb0a415cd7cd4a0d5
CaioEnterra <CaioEnterra>
2018-10-18 21:44:24 -0300

    Botoes OK

--- src/controller/CidadeController.java
+++ src/controller/CidadeController.java
@@ -13,95 +13,89 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import listcontroller.CidadeListController;
 import model.Cidade;
 
-
 public class CidadeController extends Controller<Cidade> {
-	
+
 	private Cidade cidade;
-	
-	
+
 	@FXML
-    private Label lbNome;
+	private Label lbNome;
 
-    @FXML
-    private TextField tfNome;
+	@FXML
+	private TextField tfNome;
 
-    @FXML
-    private Label lbEsdado;
+	@FXML
+	private Label lbEsdado;
 
-    @FXML
-    private TextField tfEsdado;
+	@FXML
+	private TextField tfEsdado;
 
-    @FXML
-    private Label lbPais;
+	@FXML
+	private Label lbPais;
 
-    @FXML
-    private TextField tfPais;
+	@FXML
+	private TextField tfPais;
 
-    @FXML
-    private Button btLimpar;
+	@FXML
+	private Button btLimpar;
 
-    @FXML
-    private Button btExcluir;
+	@FXML
+	private Button btExcluir;
 
-    @FXML
-    private Button btAlterar;
+	@FXML
+	private Button btAlterar;
 
-    @FXML
-    private Button btIncluir;
+	@FXML
+	private Button btIncluir;
 
-    @FXML
-    void hadleIncluir(ActionEvent event) {
-    	
-    	
+	@FXML
+	void hadleIncluir(ActionEvent event) {
+
 		getCidade().setNome(tfNome.getText());
 		getCidade().setEstado(tfEsdado.getText());
 		getCidade().setPais(tfPais.getText());
-		
 
 		super.save(getCidade());
-		
+
 		handleLimpar(event);
+		atualizarBotoes();
+	}
 
-    }
+	@FXML
+	void handleAlterar(ActionEvent event) {
 
-    @FXML
-    void handleAlterar(ActionEvent event) {
-    	
-    	getCidade().setNome(tfNome.getText());
+		getCidade().setNome(tfNome.getText());
 		getCidade().setEstado(tfEsdado.getText());
 		getCidade().setPais(tfPais.getText());
-		
 
 		super.save(getCidade());
-		
+
 		handleLimpar(event);
+		atualizarBotoes();
+	}
 
-    }
+	@FXML
+	void handleExcluir(ActionEvent event) {
 
-    @FXML
-    void handleExcluir(ActionEvent event) {
-    	
-    	super.remove(getCidade());
+		super.remove(getCidade());
 		handleLimpar(event);
-	
-		
-    }
+		atualizarBotoes();
+	}
 
-    @FXML
-    void handleLimpar(ActionEvent event) {
-    	
-    	tfNome.setText("");
+	@FXML
+	void handleLimpar(ActionEvent event) {
+
+		tfNome.setText("");
 		tfEsdado.setText("");
 		tfPais.setText("");
 		// limpando as informacoes do cliente
 		cidade = null;
-		
+		atualizarBotoes();
+	}
 
-    }
-    
-    public Cidade getCidade() {
+	public Cidade getCidade() {
 		if (cidade == null)
 			cidade = new Cidade();
 		return cidade;
@@ -111,20 +105,41 @@ public class CidadeController extends Controller<Cidade> {
 		this.cidade = cidade;
 	}
 
-	  @FXML
-	    void handleMouseClickedNome(MouseEvent event) throws IOException {
-		  	Stage stage = new Stage();
-		  	
-		  Parent root = FXMLLoader.load(getClass().getResource("/view/ListagemCidade.fxml"));
-			
-			Scene scene = new Scene(root, 600, 600);
-			
-			stage.setTitle("Cadastro Cidade");
-			stage.setScene(scene);
-			stage.initModality(Modality.APPLICATION_MODAL);
-			
-			stage.showAndWait();
+	@FXML
+	//comunicação entre as classses
+	void handleMouseClickedNome(MouseEvent event) throws IOException {
+		
+		Stage stage = new Stage();
+		FXMLLoader loader = new FXMLLoader(getCidade().getClass().getResource("/view/ListagemCidade.fxml"));
+		Parent root = loader.load();
+		
+		//obtendo o controlador
+		CidadeListController listagem = loader.getController();
 
-	    }
+		Scene scene = new Scene(root, 600, 600);
+
+		stage.setTitle("Cadastro Cidade");
+		stage.setScene(scene);
+
+		// impede o usuatrio de mexer nas funcionalidades da tela anterior
+		stage.initModality(Modality.APPLICATION_MODAL);
+		//passando parametro para ele fechar a tela 
+		listagem.setStage(stage);
+		stage.showAndWait();
+		
+		// setando a cidade
+		setCidade(listagem.getCidadeSelecionada());
+		// atualizando a interface
+		tfNome.setText(getCidade().getNome());
+		tfEsdado.setText(getCidade().getEstado());
+		tfPais.setText(getCidade().getPais());
+		
+		atualizarBotoes();
+	}
+	private void atualizarBotoes() {
+		btIncluir.setDisable(getCidade().getId() != null);
+		btAlterar.setDisable(getCidade().getId() == null);
+		btExcluir.setDisable(getCidade().getId() == null);
+	}
 
 }
--- src/listcontroller/CidadeListController.java
+++ src/listcontroller/CidadeListController.java
@@ -4,8 +4,6 @@ import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
-
-
 import factory.JPAFactory;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
@@ -19,12 +17,25 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 import model.Cidade;
 import model.Cliente;
 import repository.CidadeRepository;
 
 public class CidadeListController implements Initializable {
+
+	private Cidade cidade;
+	private Stage stage;
+
+	public Stage getStage() {
+		return stage;
+	}
+
+	public void setStage(Stage stage) {
+		this.stage = stage;
+	}
 
 	@FXML
 	private Label lbListagemDeCidades;
@@ -53,7 +64,6 @@ public class CidadeListController implements Initializable {
 	@FXML
 	private Button btPesquisar;
 
-
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// configurando as colunas das tabelas conforme os atributos da classe Cidades
@@ -63,19 +73,37 @@ public class CidadeListController implements Initializable {
 		tcPais.setCellValueFactory(new PropertyValueFactory<>("pais"));
 
 	}
-	
+
 	@FXML
 	void handleMouseClicked(MouseEvent event) {
 
+		// verificando se eh o botao principal
+		if (event.getButton().equals(MouseButton.PRIMARY)) {
+			
+			// verificando a quantidade de cliques
+			if (event.getClickCount() == 2) {
+				
+				//preenche a cidade
+				cidade = tvListagem.getSelectionModel().getSelectedItem();
+				//fecha a tela 
+				getStage().close();
+				System.out.println(cidade.getNome());
+
+			}
+		}
+
 	}
-	
+
+	public Cidade getCidadeSelecionada() {
+		return cidade;
+	}
+
 	@FXML
 	void handlePesquisar(ActionEvent event) {
-		
-		CidadeRepository repository = 
-				new CidadeRepository(JPAFactory.getEntityManager());
+
+		CidadeRepository repository = new CidadeRepository(JPAFactory.getEntityManager());
 		List<Cidade> lista = repository.getCidades(tfPesquisar.getText());
-		
+
 		if (lista.isEmpty()) {
 			Alert alerta = new Alert(AlertType.INFORMATION);
 			alerta.setTitle("Informação");
